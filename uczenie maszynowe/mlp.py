d4ef8ccdc4b117543e24264307ce5b2ebc846624
Maciej <maciejlaburda@gmail.com>
2023-12-20 08:18:43 +0100

    Adding

---
+++ uczenie maszynowe/mlp.py
@@ -0,0 +1,25 @@
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# git clone [url to repo] - klonuje repo z linku i dodaje folder na komputerze
+
+# ls - lista plików w folderze
+# cd [nazwa folderu] - komenda za pomoca ktorej wchodzi sie do folderu
+
+# git pull origin main
+
+# git commit -m "wiadomosc" - wpierdalasz zmeiany
+# git push - no wprowadzam juz zmiany
+
+
+
+
+
+
+
+
+
+
